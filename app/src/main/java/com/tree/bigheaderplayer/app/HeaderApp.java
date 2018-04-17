22723bd66d0a2e8d863d766e4b9fb7b79c0bc5b9
RookieTree <773555725qq.com>
2018-04-17 17:31:41 +0800

    提交

---
+++ app/src/main/java/com/tree/bigheaderplayer/app/HeaderApp.java
@@ -0,0 +1,21 @@
+package com.tree.bigheaderplayer.app;
+
+/*
+ *  @项目名：  BigHeaderPlayer 
+ *  @包名：    com.tree.bigheaderplayer.app
+ *  @文件名:   HeaderApp
+ *  @创建者:   Administrator
+ *  @创建时间:  2018/4/17 14:07
+ *  @描述：    TODO
+ */
+
+import android.app.Application;
+
+public class HeaderApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
